ef0ca3d1e4c0a90c71d2feea0e7ad7cc4ce3a13a
Lucien Ramond <lucien_ramond@hotmail.com>
2025-03-11 12:16:35 +0100

    #17 Fixing config issue

--- app/__init__.py
+++ app/__init__.py
@@ -25,3 +25,6 @@ from app.api.band import band_route
 app.register_blueprint(band_route)
 from app.api.instrument import instrument_route
 app.register_blueprint(instrument_route)
+
+if '__name__' == '__main__':
+    app.run()
