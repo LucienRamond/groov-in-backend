587cc4c0599f3182cbb1296d2ccde0aeda01a2f3
Lucien Ramond <lucien_ramond@hotmail.com>
2025-03-11 12:08:16 +0100

    #17 Fixing config issue

--- config.py
+++ config.py
@@ -1,17 +1,6 @@
 import os
 
-class Config:
+class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-class TestingConfig(Config):
-    TESTING = True
-
-class ProductionConfig(Config):
-    DEBUG = False
-    TESTING = False
