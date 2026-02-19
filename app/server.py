7bd194892e3dfab3e90f70bf1a69be22ef10023a
Lucien Ramond <lucien_ramond@hotmail.com>
2026-02-19 22:41:38 +0100

    Fixing import issues

--- app/server.py
+++ app/server.py
@@ -13,22 +13,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DEPLOY_DATABASE_URI")
 
 db = SQLAlchemy(app)
 
-from app.model.band import Band
-from app.model.user import User
-from app.model.instrument import Instrument
-from app.model.band_members import BandMembers
-from app.model.user_instruments import UserInstruments
+from model.band import Band
+from model.user import User
+from model.instrument import Instrument
+from model.band_members import BandMembers
+from model.user_instruments import UserInstruments
 
 with app.app_context():
     db.create_all()
 
 CORS(app, supports_credentials=True)
 
-from app.api.user import user_route
+from api.user import user_route
 app.register_blueprint(user_route)
-from app.api.band import band_route
+from api.band import band_route
 app.register_blueprint(band_route)
-from app.api.instrument import instrument_route
+from api.instrument import instrument_route
 app.register_blueprint(instrument_route)
 
 if '__name__' == '__main__':
