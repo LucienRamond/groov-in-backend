bfddc584ae98e87603cb3e2c279cc36ca670995f
Lucien Ramond <lucien_ramond@hotmail.com>
2026-02-20 11:24:05 +0100

    #19 Return user from login api

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -128,9 +128,11 @@ class UserService():
                 return make_response({'message': 'User not found'}, 404)
             else:
                 payload = {"_id": str(user.id), 'exp': datetime.datetime.now() + datetime.timedelta(minutes=60)}
+                response = User.get_user_by_id(user.id)
+                
                 if check_password_hash(user.password_hash, user_credentials["password"]):
                     token = generate_token(payload, SECRET)
-                    response = make_response({"id": user.id, "name": user.name}, 200)
+                    response = make_response(response, 200)
                     response.set_cookie("token",token)
                     return response
                 else:
